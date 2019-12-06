883dba34cc2448a4a3e94d52965c1a1bd9446499
Lauren <lauren@wnkinc.co>
2019-12-06 14:29:15 -0800

    First pass on variables and method implementations in Bullet class

--- Bullet.cs
+++ Bullet.cs
@@ -6,5 +6,41 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
+  private Rigidbody _rigidbody => GetComponent<Rigidbody>();
+  [SerializeField] private float _speed;
+  [SerializeField] private float _lifetime;
+  private float _refreshTime;
+  public bool HasExpired => Time.time >= _refreshTime;
 
+#region MonoBehaviour Methods
+  private void OnEnable()
+  {
+    // Set the prefab's transform to be that of the spawnpoint's transform
+    // Track lapsedtime
+    SetTransform(BulletPool.Instance.Spawnpoint);
+    _refreshTime = Time.time + _lifetime;
+  }
+
+  private void Update()
+  {
+    // Travel in the forward position of the spawnpoint
+    _rigidbody.velocity = transform.forward * _speed;
+  }
+
+  private void OnDisable()
+  {
+    // Hide gameObject
+    // Return to the object pool
+    gameObject.SetActive(false);
+    BulletPool.Instance.ReturnToPool(this);
+  }
+#endregion
+
+#region Class Methods
+  private void SetTransform(Transform spawnpoint)
+  {
+    transform.position = spawnpoint.position;
+    transform.rotation = spawnpoint.rotation;
+  }
+#endregion
 }
